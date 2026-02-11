affef5151572b82e74c7c13043ade23de3eb7071
hizkiarenat <renathizkia16@gmail.com>
2026-02-11 16:48:04 +0700

    EUREKA: config eureka for all Microservices

--- service-regis/src/main/java/com/example/ServiceRegisApplication.java
+++ service-regis/src/main/java/com/example/ServiceRegisApplication.java
@@ -2,14 +2,14 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class CompanyServiceApplication {
+@EnableEurekaServer
+public class ServiceRegisApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CompanyServiceApplication.class, args);
+		SpringApplication.run(ServiceRegisApplication.class, args);
 	}
 
 }
